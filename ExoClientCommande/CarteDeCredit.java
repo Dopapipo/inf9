1e31224d7cd00776d41ad75d9f7f30bfee7d7a1c
Lucian Lisnic <lisniclucian@gmail.com>
2024-02-13 12:07:16 +0100

    panier ok full

--- ExoClientCommande/CarteDeCredit.java
+++ ExoClientCommande/CarteDeCredit.java
@@ -6,4 +6,24 @@ public class CarteDeCredit {
     int numero;
     int cvv;
     Date dateExpiration;
+    float argentDisponible;
+
+    public boolean payer(float amount) {
+        if (argentDisponible >= amount) {
+            argentDisponible -= amount;
+            return true;
+        }
+        return false;
+    }
+
+    public boolean verifierCarte(int numero, int cvv, Date dateExpiration) {
+        return this.numero == numero && this.cvv == cvv && this.dateExpiration.equals(dateExpiration);
+    }
+    public boolean deposerArgent(int argent) {
+        if (argent < 0) return false; // On ne peut pas déposer un montant négatif (retrait d'argent
+        argentDisponible += argent;
+        return true;
+    }
+    
+
 }
--- ExoClientCommande/Panier.java
+++ ExoClientCommande/Panier.java
@@ -1,19 +1,40 @@
 package ExoClientCommande;
 
 import java.util.List;
+import java.util.Map;
 
 public class Panier {
-    List<Produit> produits;
+    Map<Produit, Integer> produits;
 
     public float getTotalTTC() {
         float total = 0;
-        for (Produit produit : produits) {
-            total+= produit.getPrix();
+        for (Produit produit : produits.keySet()) {
+            total += produit.getPrix() * produits.get(produit);
         }
         return total;
     }
 
-    public List<Produit> getProduits() {
+    public Map<Produit,Integer> getProduits() {
         return produits;
+    }
+
+    public void ajouterProduit(Produit produit) {
+        this.ajouterProduit(produit,1);
+    }
+
+    public void retirerProduit(Produit produit) {
+        this.retirerProduit(produit,1);
+    }
+    public void retirerProduit(Produit produit, int quantite) {
+        if (this.produits.containsKey(produit)) {
+            this.produits.put(produit, this.produits.get(produit) - quantite);
+            if (this.produits.get(produit) <= 0) {
+                this.produits.remove(produit);
+            }
+        }
+    }
+    public void ajouterProduit(Produit produit, int quantite) {
+        this.produits.putIfAbsent(produit, 0);
+        this.produits.put(produit, this.produits.get(produit) + quantite);
     }
 }
